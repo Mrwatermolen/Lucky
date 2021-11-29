f863f2e530ba32c63fe6ddf2d10dd07c7c9e30b0
Cranberries <44085002+Mrwatermolen@users.noreply.github.com>
2021-11-29 22:29:34 +0800

    改进了判断逻辑（顺带用一下在力扣上刷题的代码

--- autoFixBluRayDiscDir.py
+++ autoFixBluRayDiscDir.py
@@ -3,56 +3,112 @@
 # datetime:2021/10/29
 import os
 import sys
-import tkinter
-from tkinter import filedialog
-
-dir_mode = ['os.F_OK', 'os.R_OK', 'os.W_OK']
-dir_status = [False, False, False]  # exist, read, write
-bluRayStruct = {
-    'BDMV': {
-        'self': ['', 'AUXDATA', 'BACKUP', 'BDJO',
-                 'CLIPINF', 'JAR', 'META', 'PLAYLIST', 'STREAM'],
-        'BACKUP': ['', 'BDJO', 'CLIPINF', 'JAR', 'PLAYLIST']
-    },
-    'CERTIFICATE': {'self': ['', 'BACKUP']}
-}
-bluRaySource = ''
-missing = []
-
-if 'win' in sys.platform:
-    root = tkinter.Tk()
-    root.withdraw()
-    bluRaySource = filedialog.askdirectory()
-else:
-    bluRaySource = input("请输入需要检查的蓝光光盘目录")
 
 
-for index, authority in enumerate(dir_mode):
-    if not os.access(bluRaySource, eval(authority)):
-        print(f"dir: {bluRaySource} status: {authority} is {False}")
-        break
-    dir_status[index] = True
-if dir_status[0] and dir_status[1] and dir_status[2]:
-    for folder in bluRayStruct:
-        first_path = os.path.join(bluRaySource, folder)
-        for item in bluRayStruct[folder]:
-            second_path = (first_path) if (item == 'self') else (
-                os.path.join(first_path, item))
-            for final_item in bluRayStruct[folder][item]:
-                valid_path = os.path.join(second_path, final_item)
-                if not os.path.exists(valid_path):
-                    missing_dir = os.path.join(os.path.join(
-                        folder, item.replace('self', '')), final_item)
-                    if missing_dir == os.path.join(folder, ''):
-                        print(f"{bluRaySource}该目录非蓝光光盘目录")
-                        for i in range(len(missing)):
-                            print(f"已创建的文件夹{missing[i]}")
-                        exit(0)
-                    os.mkdir(valid_path)
-                    missing.append(missing_dir)
-    if missing == []:
-        print(f"{bluRaySource}的蓝光光盘目录结构完整")
+def simplifyPath(path: str) -> str:
+    leng = len(path)
+    if leng == 1:
+        return "/"
+
+    idx = 0
+    pathlist0 = path.split('/')
+    pathlist1 = []
+    for i in pathlist0:
+        if i and i != '.':
+            pathlist1.append(i)
+
+    pathlist0 = []
+    for i in pathlist1:
+        if i != '..':
+            idx += 1
+            pathlist0.append(i)
+        else:
+            if idx > 0:
+                pathlist0.pop()
+                idx -= 1
+
+    if idx <= 0:
+        return "/"
     else:
-        print(f"{bluRaySource}的蓝光光盘目录结构缺失")
-        for i in range(len(missing)):
-            print(f"修复缺失文件夹{missing[i]}")
+        res = "/"
+        for i in range(idx):
+            if i < idx-1:
+                res += pathlist0[i] + '/'
+            else:
+                res += pathlist0[i]
+    return res
+
+
+if __name__ == '__main__':
+    # BDMV/AUXDATA
+    # BDMV/BACKUP
+    # BDMV/BDJO
+    # BDMV/CLIPINF
+    # BDMV/JAR
+    # BDMV/META
+    # BDMV/PLAYLIST
+    # BDMV/STREAM
+    # BDMV/BACKUP/BDJO
+    # BDMV/BACKUP/CLIPINF
+    # BDMV/BACKUP/JAR
+    # BDMV/BACKUP/PLAYLIST
+    # CERTIFICATE/BACKUP
+    dir_mode = ['os.F_OK', 'os.R_OK', 'os.W_OK']
+    dir_status = [False, False, False]  # exist, read, write
+    bluRayStruct = {
+        'BDMV': {
+            '.': ['', 'AUXDATA', 'BACKUP', 'BDJO',
+                  'CLIPINF', 'JAR', 'META', 'PLAYLIST', 'STREAM'],
+            'BACKUP': ['BDJO', 'CLIPINF', 'JAR', 'PLAYLIST']
+        },
+        'CERTIFICATE': {'.': ['', 'BACKUP']}
+    }
+    bluRay_source = ''
+    missing = []
+    is_bluRay = True
+
+    try:
+        import tkinter
+        from tkinter import filedialog
+        root = tkinter.Tk()
+        root.withdraw()
+        bluRay_source = filedialog.askdirectory()
+    except:
+        bluRay_source = input("请输入需要检查的蓝光光盘目录")
+
+    for index, authority in enumerate(dir_mode):
+        if not os.access(bluRay_source, eval(authority)):
+            print(f"dir: {bluRay_source} status: {authority} is {False}")
+            break
+        dir_status[index] = True
+
+    if dir_status[0] and dir_status[1] and dir_status[2]:
+        for folder in bluRayStruct:
+            first_path = os.path.join(bluRay_source, folder)
+
+            if not os.path.exists(first_path):
+                print(f"检查目录'{bluRay_source}' 缺少{folder}文件夹，该目录非蓝光光盘目录")
+                for i in range(len(missing)):
+                    print(f"已创建的文件夹{missing[i]}")
+                is_bluRay = False
+                break
+
+            for second_folder in bluRayStruct[folder]:
+                second_path = os.path.join(first_path, second_folder)
+
+                for final_item in bluRayStruct[folder][second_folder]:
+                    valid_path = os.path.join(second_path, final_item)
+
+                    if not os.path.exists(valid_path):
+                        missing_dir = os.path.join(os.path.join(
+                            folder, second_folder), final_item)
+                        os.mkdir(valid_path)
+                        missing.append(missing_dir)
+        if missing == [] and is_bluRay:
+            print(f"{bluRay_source}的蓝光光盘目录结构完整")
+        elif is_bluRay:
+            print(f"{bluRay_source}的蓝光光盘目录结构缺失")
+            for i in range(len(missing)):
+                m = simplifyPath(
+                    '/' + missing[i].replace('\\', '/'))  # for win
+                print(f"修复缺失文件夹{m[1:]}")
